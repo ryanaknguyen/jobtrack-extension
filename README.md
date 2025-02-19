87f0be1a5bc37ba37c5e5bbaae39775abbc3e1d3
Ryan Nguyen <ryanaknguyen@gmail.com>
2025-02-18 18:21:23 -0800

    add html, css, and js files to incorporate basic input and save functionality using a text box

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# job-tracking
+# JobTrack Chrome Extension
+
+This repo serves as the source code for JobTrack, a chrome extension created by Ryan Nguyen aimed to track and save job applications.
---
+++ index.js
@@ -0,0 +1,24 @@
+// declare an array used to store the postings and the text input
+let mySavedPostings = []
+const inputEl = document.getElementById("input-el")
+const buttonInput = document.getElementById("input-button")
+const postingsList = document.getElementById("postings-list")
+
+// add the posting to the array to save it
+buttonInput.addEventListener("click", function() {
+  mySavedPostings.push(inputEl.value)
+  inputEl.value = ""
+  renderPostings();
+})
+
+// display the list of postings
+function renderPostings() {
+  let postings = ""
+  for (const posting of mySavedPostings) {
+    postings += `
+      <li>
+        <a target='_blank' href='${posting}'>${posting}</a>
+      </li>`
+  }
+  postingsList.innerHTML = postings
+}
